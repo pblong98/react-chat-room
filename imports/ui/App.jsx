8bdaa8e28e505307d5770fc51df5b069a8ad1914
pblong <32814141+pblong98@users.noreply.github.com>
2019-07-03 14:06:05 +0700

    Them logout, hien username sau khi dang nhap

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ChatBox from './Components/ChatBox';
 import InputBar from './Components/InputBar';
 import AccountUI from './Components/AccountUI';
+import { Accounts } from 'meteor/accounts-base';
 
 class App extends React.Component{
   
@@ -18,13 +19,24 @@ class App extends React.Component{
   onLogged()
   {
     this.setState({logged : true});
+    console.log(Accounts.user());
   }
 
   render()
   {
     return(
       <div>
-        { !this.state.logged ? <AccountUI logged = {this.onLogged}/> :  <div><ChatBox />  <InputBar /></div> }
+        { 
+        !this.state.logged ? <AccountUI logged = {this.onLogged}/> :  
+        <div>
+          <ChatBox />
+          <InputBar />
+          <div className="btn-group logoutbtn" role="group">
+            <button type="button" className="btn btn-success">{Accounts.user().username}</button>
+            <button className="btn btn-danger" type="button">Logout</button>
+          </div>
+        </div> 
+        }
       </div>
     );
   }
