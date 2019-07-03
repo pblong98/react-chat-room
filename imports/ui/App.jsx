9ba6cb00c826bf04cef5de81e26cc0b3796139aa
pblong <32814141+pblong98@users.noreply.github.com>
2019-07-03 13:43:20 +0700

    Tao giao dien, them login, logout, signup

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,12 +3,32 @@ import ChatBox from './Components/ChatBox';
 import InputBar from './Components/InputBar';
 import AccountUI from './Components/AccountUI';
 
-const App = () => (
-  <div>
-    <AccountUI />
-    {/* <ChatBox /> */}
-    {/* <InputBar /> */}
-  </div>
-);
+class App extends React.Component{
+  
+  constructor()
+  {
+    super();
+    this.state = {
+      logged : false
+    };
+
+    this.onLogged = this.onLogged.bind(this);
+  }
+
+  onLogged()
+  {
+    this.setState({logged : true});
+  }
+
+  render()
+  {
+    return(
+      <div>
+        { !this.state.logged ? <AccountUI logged = {this.onLogged}/> :  <div><ChatBox />  <InputBar /></div> }
+      </div>
+    );
+  }
+  
+}
 
 export default App;
--- imports/ui/Components/AccountUI.js
+++ imports/ui/Components/AccountUI.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {} from './styles/AccountUI.css'
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 class AccountUI extends React.Component
 {
@@ -7,6 +9,53 @@ class AccountUI extends React.Component
         super();
         this.state = {
             SignUp : false
+        }
+        this.formSubmit = this.formSubmit.bind(this);
+    }
+
+    formModeToggle()
+    {
+        var signUpMode = !this.state.SignUp;
+        this.setState({SignUp : signUpMode});
+    }
+
+    formSubmit(event)
+    {
+        event.preventDefault();
+        var username = this.refs.userNametxt.value;
+        var password = this.refs.passwordtxt.value;
+        var password2 = this.refs.password2txt ? this.refs.password2txt.value : "";
+        if(this.state.SignUp)
+        {
+            if(password != password2)
+            {
+                alert('FAIL: Passwords do not match');
+                return;
+            }
+            Accounts.createUser({username : username, password : password},(error, result)=>{
+                if(error)
+                {
+                    alert('FAIL: ' + error.reason);
+                }
+                else
+                {
+                    alert('Success');
+                    this.setState({SignUp : false});
+                }
+            });
+        }
+        else
+        {
+            Meteor.loginWithPassword(username, password,(error, result)=>{
+                if(error)
+                {
+                    alert('FAIL: ' + error.reason);
+                }
+                else
+                {
+                    this.props.logged();
+                }
+            });
         }
     }
 
@@ -21,13 +70,13 @@ class AccountUI extends React.Component
                     <h2>{ !this.state.SignUp ? 'Sign in' : 'Sign up'}</h2>
                     </div>
                     {/* Login Form */}
-                    <form>
-                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="User name..." />
-                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="Password..." />
+                    <form onSubmit = {this.formSubmit}>
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="User name..."  ref="userNametxt"/>
+                        <input type="password" id="password" className="fadeIn third" name="login" placeholder="Password..." ref="passwordtxt"/>
                         {
-                            this.state.SignUp ? <input type="text" id="password2" className="fadeIn third" name="login" placeholder="Type password again..."/> : ''
+                            this.state.SignUp ? <input type="password" id="password2" className="fadeIn first" name="login" placeholder="Type password again..." ref="password2txt"/> : ''
                         }
-                        <button type="button" className="btn p-1 btn-warning text-white fadeIn fourth">{ this.state.SignUp ? 'Đăng nhập' : 'Đăng ký'}</button>
+                        <button onClick={() => {this.formModeToggle()}} type="button" className="btn p-1 btn-warning text-white fadeIn fourth">{ this.state.SignUp ? 'Đăng nhập' : 'Đăng ký'}</button>
                         <input type="submit" className="fadeIn fourth"/>
                     </form>
                     {/* Remind Passowrd */}
